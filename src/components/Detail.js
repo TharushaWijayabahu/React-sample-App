335072225e820ab88e7fdd009c81351fd6f718c8
Tharusha01 <tharushaishan26@gmail.com>
2019-07-25 10:40:27 +0530

    Details page added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
+import '../../node_modules/bootstrap/dist/css/bootstrap.css'
 
-class Detail extends Component{
-    render(){
-        return(
-            <div className='detail'>
+class Detail extends Component {
+    viewMethod =() =>{
+        alert("Not implemented...!")
+    }
+    render() {
+        return (
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 text-center">
+                        <div class="card" width= '250px'>
+                            <img class="card-img-top" src="" alt=""
+                                width= '249px' height= '250px' />
+                            <div class="card-body text-center">
+                                <h4 class="card-title">Name</h4>
 
-
-
+                                <div class="text-center">
+                                    <p class="text-center"> Conducted by : </p>
+                                </div>
+                                <div class="text-center">
+                                    <button id="update" class="btn btn-primary"
+                                        onClick={this.viewMethod}>View More Details
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,44 +1,19 @@
 import React, { Component } from 'react';
-/* import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Contact from './Contact';
-import About from './About';
-import Notfound from './notfound';
-import App from '../App'; */
 
 
-class NavBar extends Component{
+class NavBar extends Component {
   render() {
     return (
 
-      /* <Router>
-        <div>
-          <h2>Welcome to React Router Tutorial</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-            <li><Link to={'/about'} className="nav-link">About</Link></li>
-          </ul>
-          </nav>
-          <hr />
-          <Switch>
-              <Route exact path='/' component={App} />
-              {<Route path='/contact' component={Contact}/>}
-              {<Route path='/about' component={About} /> }
-              {<Route path='/notfound' component={Notfound}/>}
-          </Switch>
-        </div>
-      </Router> */
-      
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
         <div className="container">
           <a href="/" className="navbar-brand" >
-            <img src="https://creativenerds.co.uk/wp-content/uploads/2017/09/badge-icon.jpg" 
-            alt="" height = "50px" width ="50px"/></a>
+            <img src="https://creativenerds.co.uk/wp-content/uploads/2017/09/badge-icon.jpg"
+              alt="" height="50px" width="50px" /></a>
 
-          <button className="navbar-toggler" type="button" data-toggle="collapse" 
-          data-target="#navbarResponsive" aria-controls="navbarResponsive" 
-          aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-toggle="collapse"
+            data-target="#navbarResponsive" aria-controls="navbarResponsive"
+            aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarResponsive">
